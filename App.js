2fe6b25231f262fdddde5d367aef63a8e549a07b
Wateryyys <tu.thanadhon_st@tni.ac.th>
2023-08-05 11:36:10 +0700

    Second Commit [Week6-August-05-2023]

--- App.js
+++ App.js
@@ -2,12 +2,16 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import TouchableExample from './components/TouchableExample'
 import RecapTouchable from './components/RecapTouchable'
+import Example_UseEffect from './components/Example_UseEffect'
+import UseEffectFlatList from './components/UseEffectFlatList'
 
 const App = () => {
   return (
     <View>
       {/* <TouchableExample/> */}
-      <RecapTouchable/>
+      {/* <RecapTouchable/> */}
+      {/* <Example_UseEffect/> */}
+      <UseEffectFlatList/>
     </View>
   )
 }
