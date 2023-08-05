752e71756b87bf99bfd4c8fc0a1d0384af885e55
Wateryyys <tu.thanadhon_st@tni.ac.th>
2023-08-05 09:38:09 +0700

    Recap Touchable [Week6-August-05-2023]

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import TouchableExample from './components/TouchableExample'
+import RecapTouchable from './components/RecapTouchable'
 
 const App = () => {
   return (
     <View>
-      <TouchableExample/>
+      {/* <TouchableExample/> */}
+      <RecapTouchable/>
     </View>
   )
 }
