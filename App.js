fedd2d35d115b59be57d1a6b10e3dc83b3add526
Wateryyys <tu.thanadhon_st@tni.ac.th>
2023-08-19 10:12:43 +0700

    First Commit [Lecture7-August-19-2023]

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import TouchableExample from './components/TouchableExample'
 import RecapTouchable from './components/RecapTouchable'
 import Example_UseEffect from './components/Example_UseEffect'
 import UseEffectFlatList from './components/UseEffectFlatList'
+import RandomUsersScreen from './components/RandomUsersScreen'
 
 const App = () => {
   return (
@@ -11,7 +12,8 @@ const App = () => {
       {/* <TouchableExample/> */}
       {/* <RecapTouchable/> */}
       {/* <Example_UseEffect/> */}
-      <UseEffectFlatList/>
+      {/* <UseEffectFlatList/> */}
+      <RandomUsersScreen/>
     </View>
   )
 }
