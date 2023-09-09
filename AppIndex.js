a4a5ac0e0c5664698ffa271d563afdba4915eaa7
Wateryyys <tu.thanadhon_st@tni.ac.th>
2023-09-09 11:29:53 +0700

    Second Commit [Lecture9-September-09-2023]

---
+++ AppIndex.js
@@ -0,0 +1,44 @@
+import { View, Text, Button } from "react-native";
+
+import React from "react";
+
+import { NavigationContainer } from "@react-navigation/native";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import IndexScreen from "./components/screens/IndexScreen";
+
+import CreatePostScreen from "./components/screens/CreatePostScreen";
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        mode="model"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#008b8b",
+          },
+
+          headerTintColor: "#ffff",
+
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      >
+        <Stack.Screen
+          name="Index"
+          component={IndexScreen}
+          options={{ title: "MainPage" }}
+        />
+
+        <Stack.Screen name="CreatePost" component={CreatePostScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
